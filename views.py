7c4c2b4aefeba01742ff749ebbe4c27493115977
Sanjeeva Maithripala <77828283+mugalan@users.noreply.github.com>
2024-02-29 18:01:37 +0530

    23,24

--- views.py
+++ views.py
@@ -15,6 +15,12 @@ def route(request):
         "base_country": settings.BASE_COUNTRY}
     return render(request, f'{local_app_name}/route.html', context)
 
+def google_map_route(request):
+    point = {'lat':6.9841644, 'lon':79.8991384}
+    context = {
+        "google_api_key": settings.GOOGLE_API_KEY,
+        "base_country": settings.BASE_COUNTRY}
+    return render(request, f'{local_app_name}/google_maps.html', context)
 
 '''
 Basic view for displaying a map 
